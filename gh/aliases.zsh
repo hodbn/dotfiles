761ff85036781ab9dc7f375af3a4062a6bc07c93
hodbn <hodbn@users.noreply.github.com>
2024-06-16 23:19:15 +0300

    feat(git): add grbom and ghpct

--- gh/aliases.zsh
+++ gh/aliases.zsh
@@ -1,3 +1,4 @@
 alias ghpc='f() { git push -u origin HEAD && gh pr create --fill $* }; f'
 alias ghpw='gh pr view -w'
 alias ghw='gh myrev && gh myprs'
+alias ghpct='ghpc --title "$(git show $(glog main..HEAD --color=always | fzf --ansi | grep -o -E "[0-9a-f]{8}") --oneline -q | cut -f2- -d" ")"'
