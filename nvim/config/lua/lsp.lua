196c2eed8564545428ea66011f9f86ec5304496e
hodbn <hodbn@users.noreply.github.com>
2025-01-19 00:42:55 +0200

    chore(nvim): update some lsp names

--- nvim/config/lua/lsp.lua
+++ nvim/config/lua/lsp.lua
@@ -1,13 +1,13 @@
 -- LSP servers configs
 local lspconfig = require('lspconfig')
 lspconfig.gopls.setup{}
-lspconfig.bufls.setup{}
+lspconfig.buf_ls.setup{}
 lspconfig.jsonls.setup{}
 lspconfig.html.setup{}
 lspconfig.cssls.setup{}
 lspconfig.vuels.setup{}
 -- lspconfig.yamlls.setup{}
-lspconfig.tsserver.setup{
+lspconfig.ts_ls.setup{
   init_options = {
     preferences = {
       disableSuggestions = true,
